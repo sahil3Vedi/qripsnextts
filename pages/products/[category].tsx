815e6994f81828d6ffdfdf80641c09779c743b72
sahil3vedi <st9800@gmail.com>
2021-05-06 15:37:17 +0530

    added filters to shelf

--- pages/products/[category].tsx
+++ pages/products/[category].tsx
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from 'react'
 // NEXT
 import { useRouter } from 'next/router'
 // ANT
-import { Button, Modal, Table, Spin, Space, message } from 'antd'
+import { Button, Modal, Table, Spin, Space, message,Checkbox, Slider, Row, Col, Empty } from 'antd'
+import { FilterOutlined, CloseCircleOutlined } from '@ant-design/icons'
 // COMPONENTS
 import Navbar from '../../components/navbar'
 import GlobalStyle from '../../stylesheets/globalStyle'
@@ -12,6 +13,10 @@ import ItemPreview from '../../components/itemPreview'
 import axios from 'axios'
 // CSS
 import shelfStyles from '../../stylesheets/shelf.module.css'
+
+const categoryMap = {
+    "cheese":["Blocks","Shredded","Slices","Dips"]
+}
 
 function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -22,6 +27,7 @@ function capitalizeGroup(str) {
 }
 
 const Products = () => {
+
     const router = useRouter()
     const category = router.query.category
     const [currentRoute, setCurrentRoute] = useState('')
@@ -29,41 +35,102 @@ const Products = () => {
     const [products, setProducts] = useState([])
     const [filteredProducts, setFilteredProducts] = useState([])
 
+    const [filterVisible, setFilterVisible] = useState(false)
+    const [filterCategories, setFilterCategories] = useState([])
+    const [filterCompanies, setFilterCompanies] = useState([])
+    const [minFilterPrice, setMinFilterPrice] = useState(100)
+    const [maxFilterPrice, setMaxFilterPrice] = useState(1500)
+    const [categories, setCategories] = useState([])
+    const [companies, setCompanies] = useState([])
+
     useEffect(() => {
-        console.log(category)
         axios.get(`${process.env.NEXT_PUBLIC_BACKEND}dummy/fetchAll/${window.location.pathname.split('/')[2]}`)
         .then(res=>{
+            let product_companies = res.data.message.map(d=>d.company).filter((v, i, a) => a.indexOf(v) === i)
             setLoadingProducts(true)
             setProducts(res.data.message)
             setFilteredProducts(res.data.message)
             setLoadingProducts(false)
+            setCompanies(product_companies)
+            setCategories(categoryMap[window.location.pathname.split('/')[2]] ? categoryMap[window.location.pathname.split('/')[2]] : [])
         })
         .catch(e=>{
             console.log(e)
         })
     }, [category])
 
-    console.log(products)
-    console.log(filteredProducts)
-  return (
-      <div>
-          <GlobalStyle/>
-          <Navbar/>
-          <div className="pageContent">
-              <p className="pageTitle">{capitalizeGroup(router.query.category)}</p>
-              {
-                loadingProducts ?
-                <div className="div-spinner"><Spin/></div>
-                :
-                <div className={shelfStyles.shelf}>
-                {
-                    products.map(d=><ItemPreview key={d._id} data={d}/>)
-                }
-                </div>
+    const handleFilterPrice = (value) => {
+        setMinFilterPrice(value[0])
+        setMaxFilterPrice(value[1])
+    }
+
+    const applyFilter = () => {
+        let temp_products = []
+        for (var product in products){
+            let productInstance = products[product]
+            if ( (productInstance.unit_price>=minFilterPrice) && (productInstance.unit_price<=maxFilterPrice) && (!filterCompanies.length || filterCompanies.includes(productInstance.company)) && (!filterCategories.length || filterCategories.some(i=>productInstance.tags.includes(i))) ){
+                temp_products.push(productInstance)
             }
-          </div>
-      </div>
-  )
+        }
+        setFilteredProducts(temp_products)
+        setFilterVisible(false)
+        message.success("Filter Applied")
+    }
+
+    const clearFilter = () => {
+        setFilteredProducts(products)
+        message.success("Filter Removed")
+    }
+
+    const filter = (
+        <Modal centered title="Filter Products" visible={filterVisible} onOk={()=>applyFilter()} onCancel={()=>setFilterVisible(false)}>
+            <p className={shelfStyles.filterTypeTitle} style={{marginTop: 0}}>Category</p>
+            <Checkbox.Group style={{ width: '100%' }} onChange={setFilterCategories}>
+                {categories.map(d=><Row key={d}><Col><Checkbox value={d.toLowerCase()}>{d}</Checkbox></Col></Row>)}
+            </Checkbox.Group>
+            <p className={shelfStyles.filterTypeTitle}>Company</p>
+            <Checkbox.Group style={{ width: '100%' }} onChange={setFilterCompanies}>
+                {companies.map(d=><Row key={d}><Col><Checkbox value={d}>{d}</Checkbox></Col></Row>)}
+            </Checkbox.Group>
+            <p className={shelfStyles.filterTypeTitle}>Price</p>
+            <Slider range step={100} defaultValue={[100, 1500]} min={100} max={1500} onChange={handleFilterPrice} tipFormatter={(value)=>`₹ ${value}`}/>
+        </Modal>
+    )
+
+    return (
+        <div>
+            {filter}
+            <GlobalStyle/>
+            <Navbar/>
+            <div className="pageContent">
+                <p className="pageTitle">{capitalizeGroup(router.query.category)}</p>
+                <div className={shelfStyles.filterDiv}>
+                    <div>{products.length ? <Button type="primary" icon={<FilterOutlined/>} onClick={()=>setFilterVisible(true)}>Filter</Button> : null}</div>
+                    <div></div>
+                    <div>
+                        {
+                            products.length===filteredProducts.length
+                            ?
+                            null
+                            :
+                            <Button danger={true} type="primary" icon={<CloseCircleOutlined/>} onClick={()=>clearFilter()}>Clear Filter</Button>
+                        }
+                    </div>
+                </div>
+                {
+                    loadingProducts ?
+                    <div className="div-spinner"><Spin/></div>
+                    :
+                    (
+                        filteredProducts.length ?
+                        <div className={shelfStyles.shelf}>{filteredProducts.map(d=><ItemPreview key={d._id} data={d}/>)}</div>
+                        :
+                        <Empty className={shelfStyles.notFound} image={Empty.PRESENTED_IMAGE_SIMPLE} description={<p>No Products Found</p>}/>
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Products
