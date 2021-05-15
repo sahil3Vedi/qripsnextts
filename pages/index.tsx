9fb3789fe2285344c975670fa9329f5618001097
sahil3vedi <st9800@gmail.com>
2021-05-15 22:53:46 +0530

    added Search Frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 // REACT
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 // COMPONENTS
 import Navbar from '../components/navbar'
 import GlobalStyle from '../stylesheets/globalStyle'
 import Footer from '../components/footer'
 // CSS
 import homeStyles from '../stylesheets/home.module.css'
+// ANT
+import { Select, Spin } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+// AXIOS
+import axios from 'axios'
+
+interface SearchValue{
+    label: string;
+    value: string;
+}
 
 const home_categories = [
     {color:"#FFEDBB",name:"Cheese",link:"plant-based-cheese",svgurl:'https://www.svgrepo.com/show/10289/cheese.svg'},
@@ -35,16 +45,38 @@ const getCategoryDiv = (d: any) => {
 }
 
 const IndexPage = () => {
+    const [searchLoading, setSearchLoading] = useState(false)
+    const [searchOptions, setSearchOptions] = useState<SearchValue[]>([])
+    const [searchValue, setSearchValue] = useState("")
     const navbarRef = useRef<any>()
+
     const openMenu = () => {
         navbarRef['current']!.openMenu()
     }
+
+    const handleSearch = (value) => {
+        axios.post(`${process.env.NEXT_PUBLIC_BACKEND}dummy/search`,{val: value ? value : " "})
+        .then(res=>{
+            let resOptions = []
+            for(var prod in res.data.message){
+                let prodInst = res.data.message[prod]
+                let prodObj = {value:`${prodInst.company} ${prodInst.name} `,text:`${prodInst.company} ${prodInst.name} `}
+                resOptions.push(prodObj)
+            }
+            console.log(resOptions)
+            setSearchOptions(resOptions)
+        })
+    }
+
+    console.log(searchOptions)
+
     return (
         <div>
             <GlobalStyle/>
             <div className="pageWrapper">
                 <Navbar ref={navbarRef}/>
                 <div className="pageContent">
+                    <Select suffixIcon={<SearchOutlined />} allowClear showSearch onSearch={handleSearch} value={searchValue ? searchValue : null} placeholder={"Search From 1000+ Products"} style={{width: "100%", marginBottom: "20px"}} defaultActiveFirstOption={false} filterOption={false} notFoundContent={searchLoading ? <Spin size="small" /> : null} options={searchOptions}/>
                     <p className="pageTitle primaryColorUnderline">100% Vegan</p>
                     <div className={homeStyles.homeCategories}>{home_categories.map(d=>getCategoryDiv(d))}</div>
                     <div className={homeStyles.homeLink}><a onClick={openMenu}>Explore More Categories</a></div>
